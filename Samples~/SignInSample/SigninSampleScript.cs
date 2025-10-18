3b75a80f6becad66ad28265abec025d22ebbc752
HajiyevEl <70325881+HajiyevEl@users.noreply.github.com>
2025-10-18 23:13:15 +0400

    Refactor SigninSampleScript for clarity and updates

--- Samples~/SignInSample/SigninSampleScript.cs
+++ Samples~/SignInSample/SigninSampleScript.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SigninSampleScript.cs" company="Google Inc.">
+// <copyright file="SigninSampleScript.cs" company="Google Inc.">
 // Copyright (C) 2017 Google Inc. All Rights Reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,105 +13,137 @@
 //  See the License for the specific language governing permissions and
 //  limitations
 
-namespace SignInSample {
-  using System;
-  using System.Collections.Generic;
-  using System.Threading.Tasks;
-  using Google;
-  using UnityEngine;
-  using UnityEngine.UI;
+namespace SignInSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Google;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-  public class SigninSampleScript : MonoBehaviour {
+    public class SigninSampleScript : MonoBehaviour
+    {
 
-    public Text statusText;
+        public Text statusText;
 
-    public string webClientId = "<your client id here>";
+        public string webClientId = "<your client id here>";
+        // Note: For Windows/macOS/Editor testing, you must provide the Client Secret
+        public string webClientSecret = "<your client secret here>";
+        private GoogleSignInConfiguration configuration;
+
+        // Defer the configuration creation until Awake so the web Client ID
+        // Can be set via the property inspector in the Editor.
+        void Awake()
+        {
+            configuration = new GoogleSignInConfiguration
+            {
+                WebClientId = webClientId,
+                RequestIdToken = true,
+
+                // ADD CLIENT SECRET FOR DESKTOP/EDITOR SUPPORT
 #if UNITY_EDITOR || UNITY_STANDALONE
-    public string Secret;
+                ClientSecret = webClientSecret
 #endif
+            };
 
-    private GoogleSignInConfiguration configuration;
-
-    // Defer the configuration creation until Awake so the web Client ID
-    // Can be set via the property inspector in the Editor.
-    void Awake() {
-      configuration = new GoogleSignInConfiguration {
-#if UNITY_EDITOR || UNITY_STANDALONE
-            ClientSecret = Secret,
-#endif
-            WebClientId = webClientId,
-            RequestIdToken = true
-      };
-    }
-
-    public void OnSignIn() {
-      GoogleSignIn.Configuration = configuration;
-      GoogleSignIn.Configuration.UseGameSignIn = false;
-      AddStatusText("Calling SignIn");
-
-      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished,TaskScheduler.FromCurrentSynchronizationContext());
-    }
-
-    public void OnSignOut() {
-      AddStatusText("Calling SignOut");
-      GoogleSignIn.DefaultInstance.SignOut();
-    }
-
-    public void OnDisconnect() {
-      AddStatusText("Calling Disconnect");
-      GoogleSignIn.DefaultInstance.Disconnect();
-    }
-
-    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task) {
-      if (task.IsFaulted) {
-        using (IEnumerator<System.Exception> enumerator =
-                task.Exception.InnerExceptions.GetEnumerator()) {
-          if (enumerator.MoveNext()) {
-            GoogleSignIn.SignInException error =
-                    (GoogleSignIn.SignInException)enumerator.Current;
-            AddStatusText("Got Error: " + error.Status + " " + error.Message);
-          } else {
-            AddStatusText("Got Unexpected Exception?!?" + task.Exception);
-          }
+            // Set the global configuration once
+            GoogleSignIn.Configuration = configuration;
         }
-      } else if(task.IsCanceled) {
-        AddStatusText("Canceled");
-      } else  {
-        AddStatusText("Welcome: " + task.Result.DisplayName + "!");
-      }
+
+        public void OnSignIn()
+        {
+            //GoogleSignIn.Configuration = configuration;
+            //GoogleSignIn.Configuration.UseGameSignIn = false;
+            //GoogleSignIn.Configuration.RequestIdToken = true;
+            AddStatusText("Calling SignIn");
+
+            // To request a Google Sign-In (not Play Games), you might need 
+            // to modify the configuration or use a separate one for games, 
+            // but for a basic sign-in with your default config, this is sufficient.
+
+            GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished);
+        }
+
+        public void OnSignInSilently()
+        {
+            //GoogleSignIn.Configuration = configuration;
+            //GoogleSignIn.Configuration.UseGameSignIn = false;
+            //GoogleSignIn.Configuration.RequestIdToken = true;
+            AddStatusText("Calling SignIn Silently");
+
+            GoogleSignIn.DefaultInstance.SignInSilently().ContinueWith(OnAuthenticationFinished);
+        }
+
+        public void OnSignOut()
+        {
+            AddStatusText("Calling SignOut");
+            GoogleSignIn.DefaultInstance.SignOut();
+        }
+
+        public void OnDisconnect()
+        {
+            AddStatusText("Calling Disconnect");
+            GoogleSignIn.DefaultInstance.Disconnect();
+        }
+
+        internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+        {
+            if (task.IsFaulted)
+            {
+                using (IEnumerator<System.Exception> enumerator =
+                        task.Exception.InnerExceptions.GetEnumerator())
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        GoogleSignIn.SignInException error =
+                                (GoogleSignIn.SignInException)enumerator.Current;
+                        AddStatusText("Got Error: " + error.Status + " " + error.Message);
+                    }
+                    else
+                    {
+                        AddStatusText("Got Unexpected Exception?!?" + task.Exception);
+                    }
+                }
+            }
+            else if (task.IsCanceled)
+            {
+                AddStatusText("Canceled");
+            }
+            else
+            {
+                AddStatusText("Welcome: " + task.Result.DisplayName + "!");
+            }
+        }
+
+        // Commented OnGamesSignIn as it requires explicit Play Games setup, 
+        // which the fork is not primarily focused on.
+        //public void OnGamesSignIn()
+        //{
+        //    GoogleSignIn.Configuration = configuration;
+        //    GoogleSignIn.Configuration.UseGameSignIn = true;
+        //    GoogleSignIn.Configuration.RequestIdToken = false;
+
+        //    AddStatusText("Calling Games SignIn");
+
+        //    GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+        //      OnAuthenticationFinished);
+        //}
+
+        private List<string> messages = new List<string>();
+        void AddStatusText(string text)
+        {
+            if (messages.Count == 5)
+            {
+                messages.RemoveAt(0);
+            }
+            messages.Add(text);
+            string txt = "";
+            foreach (string s in messages)
+            {
+                txt += "\n" + s;
+            }
+            statusText.text = txt;
+        }
     }
-
-    public void OnSignInSilently() {
-      GoogleSignIn.Configuration = configuration;
-      GoogleSignIn.Configuration.UseGameSignIn = false;
-      GoogleSignIn.Configuration.RequestIdToken = true;
-      AddStatusText("Calling SignIn Silently");
-
-      GoogleSignIn.DefaultInstance.SignInSilently().ContinueWith(OnAuthenticationFinished,TaskScheduler.FromCurrentSynchronizationContext());
-    }
-
-
-    public void OnGamesSignIn() {
-      GoogleSignIn.Configuration = configuration;
-      GoogleSignIn.Configuration.UseGameSignIn = true;
-      GoogleSignIn.Configuration.RequestIdToken = false;
-
-      AddStatusText("Calling Games SignIn");
-
-      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished,TaskScheduler.FromCurrentSynchronizationContext());
-    }
-
-    private List<string> messages = new List<string>();
-    void AddStatusText(string text) {
-      if (messages.Count == 5) {
-        messages.RemoveAt(0);
-      }
-      messages.Add(text);
-      string txt = "";
-      foreach (string s in messages) {
-        txt += "\n" + s;
-      }
-      statusText.text = txt;
-    }
-  }
 }
